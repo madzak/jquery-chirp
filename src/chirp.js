9ff72a3614601ff4c40a20c27fa46aada9dc689f
Zak Zajac <zak@madzak.com>
2012-02-17 16:26:48 -0800

    fixed issue with function not being callable. made it more sexy with some fancy javascript

--- src/chirp.js
+++ src/chirp.js
@@ -1,21 +1,19 @@
-(function($) {
-    $(function() {
-        function fetchTweets(user, count, renderCallback) {
-            var url = 'http://search.twitter.com/search.json',
-                searchParams = {
-                    'q': 'from:' + user,
-                    'rpp': count
-                }
+;(function($) {
+    this.fetchTweets = function(user, count, renderCallback) {
+        var url = 'http://search.twitter.com/search.json',
+            searchParams = {
+                'q': 'from:' + user,
+                'rpp': count
+            }
 
-            $.ajax({
-                url: url,
-                type: 'GET',
-                data: searchParams,
-                dataType: 'jsonp',
-                success: function(search) {
-                    renderCallback(search.results);
-                }
-            });
-        };
-    });
-})(jQuery);
+        $.ajax({
+            url: url,
+            type: 'GET',
+            data: searchParams,
+            dataType: 'jsonp',
+            success: function(search) {
+                renderCallback(search.results);
+            }
+        });
+    };
+}).apply(Chirp, [jQuery]);
